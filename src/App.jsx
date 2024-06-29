1a5e2fab59e1b57ff7182fc7c11e7899be8f454e
sebastacio <sebastian.rgc@gmail.com>
2024-06-29 18:45:36 -0400

    navbar colapsada

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import NavBar from './componets/NavBar/NavBar'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
- {nombre}
-    </>
+    <> 
+   <NavBar></NavBar>
+     </>
   )
 }
 
 export default App
-const nombre = "seba"
