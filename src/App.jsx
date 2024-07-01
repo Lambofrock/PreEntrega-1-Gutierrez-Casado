6951244c4ecafa285c64682f63a13fc74822215c
sebastacio <sebastian.rgc@gmail.com>
2024-06-30 23:23:40 -0400

    integrando ilc

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import NavBar from './componets/NavBar/NavBar'
+import ItemListContainer from './componets/itemListContainer/ItemListContainer'
 
 
 
 
 function App() {
 
-const [cartCount] = useState(6)
+  const [cartCount] = useState(10)
   return (
     <>
-      <NavBar cartCount={cartCount }/>
+      <NavBar cartCount={cartCount} />
+      <ItemListContainer greetings="bienvenido a Hey Pulga!" />
     </>
   )
 }
-
+ 
 export default App
--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -6,33 +6,26 @@ console.log("holiswi")
 function NavBar({ cartCount }) {
     return (
 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div>...</div>
+                 <img className="rounded"  width="60" height="60" src="img\Logo (1).jpg" alt="logo" />
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+           
+                <a className="navbar-brand" href="#">Hey Pulga !</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#">Tienda</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
+                    
                         <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a className="nav-link disabled" aria-disabled="true">Calidad en cada fibra</a>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
