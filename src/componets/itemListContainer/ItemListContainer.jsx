767f48e5943eb68866d30cb63b9e5bb76d45beb9
sebastacio <sebastian.rgc@gmail.com>
2024-07-01 19:37:17 -0400

    styled

--- src/componets/itemListContainer/ItemListContainer.jsx
+++ src/componets/itemListContainer/ItemListContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 const categorias= ["poleron","swater","polera","niños"]
 console.log(categorias)
-function ItemListContainer() {
+function ItemListContainer({greetings}) {
   return (
-    <div></div>
+    <>
+     <h2 className=" text-light d-flex justify-content-center">{greetings}</h2>
+     <ul className='list-group '>
+     {categorias.map(category => (
+        <li className='list-group-item w-25 p-3 d-flex justify-content-center'>{category}</li>
+     ))}
+     </ul>
+    
+    </> 
   )
 }
 
